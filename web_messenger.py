5b51507ff47402f00467d0d69d9f91bbc66906cd
saltyse <saltykimstl@outlook.com>
2025-11-01 00:24:24 +0500

    Update web_messenger.py

--- web_messenger.py
+++ web_messenger.py
@@ -476,4 +476,5 @@ socketio = app.extensions['socketio']
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    # Добавляем allow_unsafe_werkzeug=True для продакшн
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
